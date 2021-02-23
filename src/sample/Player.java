967ab6c424d95a87bad1128a28e6796eb1000f10
Leong, Nok Sam <noksam.leong@gatech.edu>
2021-02-22 23:56:03 -0500

    add player class

---
+++ src/sample/Player.java
@@ -0,0 +1,46 @@
+package sample;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Player {
+    private int x;
+    private int y;
+    private String name;
+    private List<Weapon> weaponList;
+    //index of weapon in the weapon list
+    private int holdingWeapon;
+
+    public Player(String inputName, Weapon initialWeapon, int initialX, int initialY){
+        name = inputName;
+        weaponList = new ArrayList<>();
+        weaponList.add(initialWeapon);
+        x = initialX;
+        y = initialY;
+    }
+
+    public void obtainNewWeapon(Weapon newWeapon){
+        weaponList.add(newWeapon);
+    }
+
+    public void equipWeapon(Weapon target){
+        holdingWeapon = weaponList.indexOf(target);
+    }
+
+    public List<Weapon> getWeaponList() {
+        return weaponList;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getHoldingWeapon() {
+        return holdingWeapon;
+    }
+}

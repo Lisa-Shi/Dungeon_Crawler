c24a967a3c6bfff9d984ff0fec859c1d00ca98a4
Hayden Rieder <haydenrieder50@hotmail.com>
2021-02-26 14:54:37 -0500

    Add initial welcome screen class

---
+++ src/sample/WelcomeScreen.java
@@ -0,0 +1,39 @@
+package sample;
+
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+public class WelcomeScreen {
+
+    public Button startButton;
+    public Button exitButton;
+
+    public BorderPane welcomeLayout() {
+
+        BorderPane border = new BorderPane();
+
+        Label title = new Label("Dungeon Crawler");
+        Font titleFont = new Font("Times New Roman", 25);
+        title.setFont(titleFont);
+
+        VBox buttonBox = new VBox();
+        startButton = new Button("Start Game");
+        exitButton = new Button("Exit");
+        exitButton.setOnAction(e -> {
+            Stage stage = (Stage)exitButton.getScene().getWindow();
+            stage.close();
+        });
+        buttonBox.getChildren().addAll(startButton, exitButton);
+
+        border.setTop(title);
+        border.setBottom(buttonBox);
+
+        return border;
+
+    }
+}

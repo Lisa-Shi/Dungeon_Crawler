560dd80023e64ad2793095268b957ffd9134d23f
Hayden Rieder <haydenrieder50@hotmail.com>
2021-02-26 15:22:32 -0500

    Add javadocs to welcome screen class

--- src/sample/WelcomeScreen.java
+++ src/sample/WelcomeScreen.java
@@ -12,6 +12,11 @@ public class WelcomeScreen {
     public Button startButton;
     public Button exitButton;
 
+    /**
+     * Creates border pane for the welcome screen.
+     *
+     * @return borderPane that will be used in Main.java to display welcome
+     */
     public BorderPane welcomeLayout() {
 
         BorderPane border = new BorderPane();

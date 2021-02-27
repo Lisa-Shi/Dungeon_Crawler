e9a850e9c48baba24681532b32c4b3829a8e8758
Leong, Nok Sam <noksam.leong@gatech.edu>
2021-02-27 04:52:49 -0500

    add configtest
    not complete yet. wait til main is fixed

--- test/ConfigTest.java
+++ test/ConfigTest.java
@@ -10,13 +10,34 @@ import java.awt.*;
 import static org.testfx.api.FxAssert.verifyThat;
 
 public class ConfigTest extends ApplicationTest {
+    private Main game;
     @Override
     public void start(Stage primaryStage) throws Exception{
-        ConfigurationScreen config = new ConfigurationScreen();
+        game = new Main();
+        game.start();
     }
     @Test
-    public void testNoNamePlayer() {
+    public void testNoName() {
         clickOn("1: easy");
-        verifyThat("We are in the second screen now!", NodeMatchers.isNotNull());
+        clickOn("Go to Room");
+        verifyThat("please enter a name", NodeMatchers.isNotNull());
+    }
+    @Test
+    public void testNoDiff() {
+        //need to setID to FX object in the screen classes
+        clickOn("#nameInput").write("name");
+        clickOn("Go to Room");
+        verifyThat("please select difficulty", NodeMatchers.isNotNull());
+    }
+    @Test
+    public void testValid() {
+        //need to setID to FX object in the screen classes
+        clickOn("#nameInput").write("name");
+        clickOn("1: easy");
+        clickOn("Go to Room");
+        Player temp = new Player("name", None, 1);
+        ArrayList<String> list = new ArrayList<>();
+        list = {"name", "difficulty"};
+        assertions.assertthat(game.getPlayer()).comparetobyattribute(list);
     }
 }

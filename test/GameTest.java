e7c0d85a018805cff6f57b6a992bd35e0482642b
Leong, Nok Sam <noksam.leong@gatech.edu>
2021-03-01 13:29:45 -0500

    check style

--- test/GameTest.java
+++ test/GameTest.java
@@ -1,16 +1,13 @@
-import org.testfx.assertions.api.Assertions;
+import org.testfx.framework.junit.ApplicationTest;
 import sample.GameStage;
 import sample.Main;
 import javafx.stage.Stage;
 import org.junit.Test;
-import org.testfx.framework.junit.ApplicationTest;
-import org.testfx.matcher.base.NodeMatchers;
 import sample.Player;
 import sample.Vector2D;
 import sample.Weapon;
 
 import static org.junit.Assert.assertEquals;
-import static org.testfx.api.FxAssert.verifyThat;
 
 public class GameTest extends ApplicationTest {
     private GameStage game;
@@ -18,7 +15,7 @@ public class GameTest extends ApplicationTest {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        player = new Player("test", new Weapon("test","test",1,5), 0, 0, 0);
+        player = new Player("test", new Weapon("test", "test", 1, 5), 0, 0, 0);
         game = new GameStage(player);
         game.start(new Stage());
     }
@@ -28,10 +25,11 @@ public class GameTest extends ApplicationTest {
         player.getPhysics().pushLeft(Main.DEFAULT_CONTROL_PLAYER_FORCE);
         game.update();
 
-        assertEquals(-Main.DEFAULT_CONTROL_PLAYER_FORCE, player.getPhysics().getVelocity().getX(),0.001);
-        assertEquals(0, player.getPhysics().getVelocity().getY() ,0.001);
-        assertEquals(0, player.getPhysics().getAcceleration().getX(),0.001);
-        assertEquals(0, player.getPhysics().getAcceleration().getY(),0.001);
+        assertEquals(-Main.DEFAULT_CONTROL_PLAYER_FORCE,
+                player.getPhysics().getVelocity().getX(), 0.001);
+        assertEquals(0, player.getPhysics().getVelocity().getY(), 0.001);
+        assertEquals(0, player.getPhysics().getAcceleration().getX(), 0.001);
+        assertEquals(0, player.getPhysics().getAcceleration().getY(), 0.001);
     }
     @Test
     public void testPushRight() {
@@ -39,30 +37,33 @@ public class GameTest extends ApplicationTest {
         player.getPhysics().pushRight(Main.DEFAULT_CONTROL_PLAYER_FORCE);
         game.update();
 
-        assertEquals(Main.DEFAULT_CONTROL_PLAYER_FORCE, player.getPhysics().getVelocity().getX(),0.001);
-        assertEquals(0, player.getPhysics().getVelocity().getY() ,0.001);
-        assertEquals(0, player.getPhysics().getAcceleration().getX(),0.001);
-        assertEquals(0, player.getPhysics().getAcceleration().getY(),0.001);
+        assertEquals(Main.DEFAULT_CONTROL_PLAYER_FORCE,
+                player.getPhysics().getVelocity().getX(), 0.001);
+        assertEquals(0, player.getPhysics().getVelocity().getY(), 0.001);
+        assertEquals(0, player.getPhysics().getAcceleration().getX(), 0.001);
+        assertEquals(0, player.getPhysics().getAcceleration().getY(), 0.001);
     }
     @Test
     public void testPushUp() {
         player.getPhysics().pushUp(Main.DEFAULT_CONTROL_PLAYER_FORCE);
         game.update();
 
-        assertEquals(0, player.getPhysics().getVelocity().getX(),0.001);
-        assertEquals(-Main.DEFAULT_CONTROL_PLAYER_FORCE, player.getPhysics().getVelocity().getY() ,0.001);
-        assertEquals(0, player.getPhysics().getAcceleration().getX(),0.001);
-        assertEquals(0, player.getPhysics().getAcceleration().getY(),0.001);
+        assertEquals(0, player.getPhysics().getVelocity().getX(), 0.001);
+        assertEquals(-Main.DEFAULT_CONTROL_PLAYER_FORCE,
+                player.getPhysics().getVelocity().getY(), 0.001);
+        assertEquals(0, player.getPhysics().getAcceleration().getX(), 0.001);
+        assertEquals(0, player.getPhysics().getAcceleration().getY(), 0.001);
     }
     @Test
     public void testPushDown() {
         player.getPhysics().pushDown(Main.DEFAULT_CONTROL_PLAYER_FORCE);
         game.update();
 
-        assertEquals(0, player.getPhysics().getVelocity().getX(),0.001);
-        assertEquals(Main.DEFAULT_CONTROL_PLAYER_FORCE, player.getPhysics().getVelocity().getY() ,0.001);
-        assertEquals(0, player.getPhysics().getAcceleration().getX(),0.001);
-        assertEquals(0, player.getPhysics().getAcceleration().getY(),0.001);
+        assertEquals(0, player.getPhysics().getVelocity().getX(), 0.001);
+        assertEquals(Main.DEFAULT_CONTROL_PLAYER_FORCE,
+                player.getPhysics().getVelocity().getY(), 0.001);
+        assertEquals(0, player.getPhysics().getAcceleration().getX(), 0.001);
+        assertEquals(0, player.getPhysics().getAcceleration().getY(), 0.001);
     }
     @Test
     public void testMaxVelocity() {

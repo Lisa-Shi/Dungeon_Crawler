d32612de67d6f52545ebf0c7d495709b5d910b3a
Hayden Rieder <haydenrieder50@hotmail.com>
2021-02-28 20:11:59 -0500

    Add tests for welcome screen

---
+++ test/WelcomeTest.java
@@ -0,0 +1,35 @@
+import javafx.stage.Stage;
+import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
+import org.testfx.matcher.base.NodeMatchers;
+import sample.Main;
+
+import static org.testfx.api.FxAssert.verifyThat;
+
+public class WelcomeTest extends ApplicationTest {
+
+    private Main game;
+
+    public void start(Stage primaryStage) throws Exception {
+        game = new Main();
+        game.start(primaryStage);
+    }
+
+    /**
+     * Tests to see if start button opens config screen.
+     */
+    @Test
+    public void testStartButton() {
+        clickOn("Start Game");
+        verifyThat("Go to room", NodeMatchers.isVisible());
+        verifyThat("Back", NodeMatchers.isVisible());
+    }
+
+    /**
+     * Tests to see if exit button closes application. (Incomplete)
+     */
+    @Test
+    public void testExitButton() {
+        clickOn("Exit").sleep(5000);
+    }
+}

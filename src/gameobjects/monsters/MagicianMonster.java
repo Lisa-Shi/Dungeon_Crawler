c11bbe25bfd8245c88df7d6d8b68f804c594bf45
Blake Miller <bmiller315@gatech.edu>
2021-04-01 19:21:55 -0700

    Start changes on Magician and Slime monsters

--- src/gameobjects/monsters/MagicianMonster.java
+++ src/gameobjects/monsters/MagicianMonster.java
@@ -5,6 +5,6 @@ import gamemap.Room;
 
 public class MagicianMonster extends Monster {
     public MagicianMonster(Room room, double initialX, double initialY) {
-        super(room, 100, 100, 10, initialX, initialY, Main.MAGICIAN_STANDING_SHEET);
+        super(room, 100, 100, 20, initialX, initialY, Main.MAGICIAN_STANDING_SHEET);
     }
 }
--- src/gameobjects/monsters/SlimeMonster.java
+++ src/gameobjects/monsters/SlimeMonster.java
@@ -8,6 +8,6 @@ import gamemap.Room;
 
 public class SlimeMonster extends Monster {
     public SlimeMonster(Room room, double initialX, double initialY) {
-        super(room, 100, 100, 10, initialX, initialY, Main.SLIME_STANDING_SHEET);
+        super(room, 50, 50, 10, initialX, initialY, Main.SLIME_STANDING_SHEET);
     }
 }

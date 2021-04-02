ad2f7b44d0ab38eceab94334b429389cfc5ee1b8
Leong, Nok Sam <noksam.leong@gatech.edu>
2021-04-02 00:57:33 -0400

    fix test

--- test/unittest/m4/MonsterTest.java
+++ test/unittest/m4/MonsterTest.java
@@ -1,7 +1,10 @@
 package unittest.m4;
 
+import gamemap.GameMap;
 import gamemap.Room;
 import gameobjects.Player;
+import gameobjects.monsters.Monster;
+import gameobjects.physics.Vector2D;
 import gameobjects.tiles.ExitTile;
 import javafx.stage.Stage;
 import main.GameStage;
@@ -9,27 +12,43 @@ import main.Weapon;
 import org.junit.Test;
 import org.testfx.framework.junit.ApplicationTest;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class MonsterTest  extends ApplicationTest {
     private GameStage game;
     private Player player;
     private Stage stage;
-
+    private GameMap map;
     @Override
     public void start(Stage primaryStage) throws Exception {
         Room r = new Room(20, 20);
         player = new Player("unittest", new Weapon("unittest", "unittest", 1, 5), r, 0, 0, 0);
+        player.setHealth(1000000);
         game = new GameStage(player, r);
-        this.stage = new Stage();
-        game.start(stage);
+        map = game.getMap();
+        game.start(new Stage());
     }
-    @Test(timeout = 5000)
-    public void noMonsterAtStart() {
+    @Test
+    public void monsterMoveTowardPlayer() {
         Room room = game.getRoom();
-        assertTrue(room.getMonsters().size() == 0);
+        ExitTile exit = room.getExits().get(0);
+        player.getPhysics().setPosition(exit.getPhysics().getPosition());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Monster monster = game.getRoom().getMonsters().get(0);
+        Vector2D old = monster.getPhysics().getPosition();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Vector2D newP = monster.getPhysics().getPosition();
+        assertFalse(newP.subtract(old).equals(new Vector2D(0, 0)));
     }
-    @Test(timeout = 5000)
+    @Test
     public void atLeastOneMonster() {
         Room room = game.getRoom();
         ExitTile exit = room.getExits().get(0);
@@ -42,4 +61,5 @@ public class MonsterTest  extends ApplicationTest {
         room = game.getRoom();
         assertTrue((room.getMonsters().size() > 0));
     }
+
 }

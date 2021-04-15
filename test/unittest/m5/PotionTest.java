a6da150d907a977e43ee4565f5d4d616cfaf3d9a
Hayden Rieder <haydenrieder50@hotmail.com>
2021-04-15 16:55:11 -0400

    Add tests

---
+++ test/unittest/m5/PotionTest.java
@@ -0,0 +1,42 @@
+package unittest.m5;
+
+import gamemap.Room;
+import gameobjects.Player;
+import javafx.stage.Stage;
+import main.GameStage;
+import main.Main;
+import main.Weapon;
+import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
+import static org.junit.Assert.assertEquals;
+
+public class PotionTest extends ApplicationTest {
+
+    private GameStage game;
+    private Player player;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Room r = new Room(20, 20);
+        player = new Player("unittest", new Weapon("unittest", "unittest",
+                1, 5), r, 3 * Main.TILE_WIDTH, 3 * Main.TILE_HEIGHT, 0);
+        game = new GameStage(player, r);
+        game.start(new Stage());
+        game.getRoom().clear();
+    }
+
+    @Test
+    public void testHealthPotion() {
+        player.setHealth(50);
+        player.consumePotion();
+        assertEquals(75, player.getHealth());
+    }
+
+    @Test
+    public void testAttackPotion() {
+        Weapon weapon = player.getWeaponList().get(player.getHoldingWeapon());
+        int originalDamage = weapon.getDamage();
+        player.consumePotion();
+        assertEquals(originalDamage * 2, weapon.getDamage());
+    }
+}

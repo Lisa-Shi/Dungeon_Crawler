bbcc570cc1f891b9b5a91726989470cab9b66fe6
Hayden Rieder <haydenrieder50@hotmail.com>
2021-04-20 19:28:04 -0400

    Add javadocs for BossMonster.java

--- src/gameobjects/monsters/BossMonster.java
+++ src/gameobjects/monsters/BossMonster.java
@@ -7,13 +7,28 @@ import gameobjects.potions.EndGameAttackPotion;
 import javafx.scene.layout.Pane;
 import main.Main;
 
+/**
+ * Final monster to be fought in the last room of the game.
+ */
 public class BossMonster extends Monster {
 
+    /**
+     * Constructor for the boss
+     * @param room room 9 that the boss is found
+     * @param initialX x value where the boss is spawned
+     * @param initialY y value where the boss is spawned
+     */
     public BossMonster(Room room, double initialX, double initialY) {
         super(room, 500, 500, 50, initialX, initialY, Main.BOSS_STANDING_SHEET);
         this.getCarryReward().put(new EndGameAttackPotion(), 6);
     }
 
+    /**
+     * Overrides the attack method for the monster class
+     * @param room the room which the monster is located
+     * @param pane overall pane
+     * @param other what to damage
+     */
     @Override
     public void attack(Room room, Pane pane, Damageable other) {
         if (inRange(other)) {
@@ -21,6 +36,11 @@ public class BossMonster extends Monster {
         }
     }
 
+    /**
+     * Method to check if the player is in range of the monster.
+     * @param other the player
+     * @return boolean of whether the monster is in range or not
+     */
     public boolean inRange(Damageable other) {
         Vector2D otherPos = other.getPhysics().getPosition();
         Vector2D thisPos = getPhysics().getPosition();
